9d844c542768ab04bc21fd82e7edaf7fe011b7ed
Eugene Leto <mholy@protonmail.ch>
2019-09-24 23:04:43 +0500

    trying to deploy 3

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a217e94b4291b5c79ddaaa8e2f969a3.js"
+  "/precache-manifest.8d0036585fe796232746ffbe482e025e.js"
 );
 
 self.addEventListener('message', (event) => {
