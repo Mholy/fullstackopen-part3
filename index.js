d50b2d898de1f438545e567248375ec23f358e24
Eugene Leto <mholy@protonmail.ch>
2019-09-29 15:30:45 +0500

    added unknown endpoint middleware

--- index.js
+++ index.js
@@ -71,6 +71,11 @@ app.post('/api/persons', (req, res) => {
     })
 })
 
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
 const errorHandler = (error, req, res, next) => {
     console.error(error.message)
 
