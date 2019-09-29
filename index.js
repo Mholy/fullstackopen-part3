522bcec3f634c785c3af997a6de93c380e98c413
Eugene Leto <mholy@protonmail.ch>
2019-09-29 15:27:41 +0500

    Exercises 3.15-3.16

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 app.use(express.static('build'))
-
 app.use(cors())
-
 app.use(bodyParser.json())
 
 morgan.token('post-body', req => JSON.stringify(req.body))
@@ -32,18 +30,24 @@ app.get('/info', (req, res) => {
     `)
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    Person.findById(req.params.id).then(person => {
-        res.json(person.toJSON())
-    })
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person.toJSON())
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -66,6 +70,18 @@ app.post('/api/persons', (req, res) => {
         res.json(savedNote.toJSON())
     })
 })
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        res.status(400).send({ error: 'malformated id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
